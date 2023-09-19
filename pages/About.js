6144243900f38b072f67965462fdfa817b9cb7e4
RaqinS <raqin2003@gmail.com>
2023-09-19 01:42:04 +0000

    added hobby cards to showcase my hobbies

--- pages/About.js
+++ pages/About.js
@@ -1,6 +1,19 @@
 import React, { useState } from 'react';
 import Whoami from './whoami';
-import './about.css'
+import './about.css';
+
+// Define your hobby data with images
+const hobbiesData = [
+  {
+    name: 'Hiking',
+    image: 'url-to-hiking-image.jpg',
+  },
+  {
+    name: 'Watching Hockey',
+    image: 'url-to-hockey-image.jpg',
+  },
+  // Add more hobbies as needed
+];
 
 function About() {
   const [showText, setShowText] = useState(false);
@@ -13,11 +26,21 @@ function About() {
       {showText && (
         <div className="about-text">
           <h1 className="about-header">About Me</h1>
-          <p>Born and raised in the picturesque landscapes of Vancouver, BC, I've always been captivated by the blend of nature's beauty and the digital world's endless possibilities.
+          <p>Born and raised in the picturesque landscapes of Vancouver, BC, I've always been captivated by the blend of nature's beauty and the digital world's endless possibilities.</p>
+          <p>My journey into technology began early, driven by a natural curiosity. Now, as a dedicated tech enthusiast with a strong focus on cloud computing and automation, I'm constantly exploring the ever-evolving tech landscape. Currently studying at BCIT, I'm gaining the skills to make a mark in the industry.</p>
+          <p>Beyond tech, I have several hobbies that I'm passionate about:</p>
 
-My journey into technology began early, driven by a natural curiosity. Now, as a dedicated tech enthusiast with a strong focus on cloud computing and automation, I'm constantly exploring the ever-evolving tech landscape. Currently studying at BCIT, I'm gaining the skills to make a mark in the industry.
-
-Beyond tech, I'm passionate about sports. Whether it's cheering for the Vancouver Canucks or hiking in the Pacific Northwest, I find inspiration in the fusion of physical and digital realms, where innovation meets athleticism..</p>
+          {/* Hobby Cards */}
+          <div className="hobby-cards">
+            {hobbiesData.map((hobby, index) => (
+              <div className="hobby-card" key={index}>
+                <div className="hobby-image">
+                  <img src={hobby.image} alt={hobby.name} />
+                </div>
+                <p className="hobby-name">{hobby.name}</p>
+              </div>
+            ))}
+          </div>
         </div>
       )}
     </div>
@@ -25,6 +48,3 @@ Beyond tech, I'm passionate about sports. Whether it's cheering for the Vancouve
 }
 
 export default About;
-
-
-
